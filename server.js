2f70968bfd970b7fd213eee4d1a29e30951e4320
darkpgx <darkpgx@gmail.com>
2014-09-23 17:21:40 -0700

    add auto start videochat

--- server.js
+++ server.js
@@ -2,32 +2,28 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'ejs');
 var OpenTok = require('opentok'),
-    opentok = new OpenTok(process.env.api_key, process.env.api_secret ); 
+    opentok = new OpenTok(process.env.opentok_key, process.env.opentok_secret ); 
 
 var room_session = {};
 
-app.get('/', function(req, res){
-  res.render('index');
-});
-
-app.get('/room/:rmname', function(req, res){
-  res.render('chatroom', {rmname: req.params.rmname});
+app.get('/:rmname', function(req, res){
+  res.render('chatroom', {rmname:req.params.rmname});
 });
 
 // app to insert videochat
 app.get('/videochat/:rmname', function(req, res){
   if (typeof room_session[req.params.rmname] === "string" && room_session[req.params.rmname] !== ""){
     var session_id = room_session[req.params.rmname];
-    var TK = opentok.generateToken(session_id);
-    res.send({api_key: process.env.api_key, session_id: session_id, token: TK});
+    var token = opentok.generateToken(session_id);
+    res.send({api_key: process.env.opentok_key, session_id: session_id, token: token});
   }
   else {
     opentok.createSession(function(err, session) {
       if (err) return console.log(err);
       var session_id = session.sessionId;
       room_session[req.params.rmname] = session_id;
-      var TK = opentok.generateToken(session_id);
-      res.send({api_key: process.env.api_key, session_id: session_id, token: TK});
+      var token = opentok.generateToken(session_id);
+      res.send({api_key: process.env.opentok_key, session_id: session_id, token: token});
     });
   };
 });
